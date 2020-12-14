14cad345b1523077c52358a3955093683215e697
Juno Brown <amarbrown@gmail.com>
2020-12-14 18:27:56 -0500

    added object for questions, choices, and answers, added buttons and centered html

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+var timeLeft = 100
 
+
+
+var questionArray = [
+{
+    "question": "Commonly used data types DO NOT Include:"
+    "answers": ["strings", "booleans", "alerts", "numbers"],
+    "correct": 2
+},
+{
+    "question": "The condition in an if/else statement is enclosed within _____."
+    "answers": ["quotes", "curlybrackets", "parentheses", "square brackets"],
+    "correct": 2
+},
+{
+    "question": "Arrays in JavaScript can be used to store ____."
+    "answers": ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    "correct": 3
+}
+{
+    "question": "String values must be enclosed within _____ when being assigned to variables."
+    "answers": ["commas", "curly brackets", "quotes", "parentheses"],
+    "correct": 3
+}
+{
+    "question": "A very useful tool used during development and debugging for printing content to the debugger is:" 
+    "answers" ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    "correct": 3
+}
+]
 
 
 // GIVEN I am taking a code quiz
