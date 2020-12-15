04eedf6c18734d534c012045197ec94f19459933
Juno Brown <amarbrown@gmail.com>
2020-12-15 17:30:56 -0500

    adding functions for clearing out the content at start of quiz as well as new id's in html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timeLeft = 100
-var startquiz = document.getElementById("startBtn")
-var header = document.getElementById("header")
+var startBtn = document.getElementById("startQuiz")
+var questions = document.getElementById("question")
 var startingPage = document.getElementById("startingPageMl")
 var questionNum = 0
 var content = document.getElementById("quizContent")
@@ -9,27 +9,27 @@ var questionArray = [
 {
     "question": "Commonly used data types DO NOT Include:",
     "answers": ["strings", "booleans", "alerts", "numbers"],
-    "correct": 2
+    "correct": "alerts"
 },
 {
     "question": "The condition in an if/else statement is enclosed within _____.",
     "answers": ["quotes", "curlybrackets", "parentheses", "square brackets"],
-    "correct": 2
+    "correct": "parentheses"
 },
 {
     "question": "Arrays in JavaScript can be used to store ____.",
     "answers": ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    "correct": 3
+    "correct": "all of the above"
 },
 {
     "question": "String values must be enclosed within _____ when being assigned to variables.",
-    "answers": ["commas", "curly brackets", "quotes", "parentheses"],
-    "correct": 3
+    "answers": ["co mmas", "curly brackets", "quotes", "parentheses"],
+    "correct": "quotes"
 },
 {
     "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
     "answers" :["JavaScript", "terminal/bash", "for loops", "console.log"],
-    "correct": 3
+    "correct": "console.log"
 },
 ]
 
@@ -37,14 +37,19 @@ var questionArray = [
 // GIVEN I am taking a code quiz
 
 // WHEN I click the start button
-startquiz.addEventListener("click", function() {
-    startingPage. ;
-
-})
+$(startBtn).on("click", function() {
+    $(startingPage).html("");
+});
 // THEN a timer starts and I am presented with a question
-
+for(var i = 0; i < questionArray.length; i++) {
+    var qEl = $("<p>");
+    // question.addClass ("col-sm-3");
+}
 // WHEN I answer a question
-
+var content = $("<h3>");
+content.textContent(questionArray[i]);
+qEl.append(h3El);
+questionArray.append(qEl)
 // THEN I am presented with another question
 
 // WHEN I answer a question incorrectly
