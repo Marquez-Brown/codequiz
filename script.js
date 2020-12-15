d15f407ecc942d76c42979d4e583f65e37883627
Juno Brown <amarbrown@gmail.com>
2020-12-14 21:49:39 -0500

    added card to html, working on getting content to clear on html after button press

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 var timeLeft = 100
-
-
+var startquiz = document.getElementById("startBtn")
+var header = document.getElementById("header")
+var startingPage = document.getElementById("startingPageMl")
+var questionNum = 0
+var content = document.getElementById("quizContent")
 
 var questionArray = [
 {
-    "question": "Commonly used data types DO NOT Include:"
+    "question": "Commonly used data types DO NOT Include:",
     "answers": ["strings", "booleans", "alerts", "numbers"],
     "correct": 2
 },
 {
-    "question": "The condition in an if/else statement is enclosed within _____."
+    "question": "The condition in an if/else statement is enclosed within _____.",
     "answers": ["quotes", "curlybrackets", "parentheses", "square brackets"],
     "correct": 2
 },
 {
-    "question": "Arrays in JavaScript can be used to store ____."
+    "question": "Arrays in JavaScript can be used to store ____.",
     "answers": ["numbers and strings", "other arrays", "booleans", "all of the above"],
     "correct": 3
-}
+},
 {
-    "question": "String values must be enclosed within _____ when being assigned to variables."
+    "question": "String values must be enclosed within _____ when being assigned to variables.",
     "answers": ["commas", "curly brackets", "quotes", "parentheses"],
     "correct": 3
-}
+},
 {
-    "question": "A very useful tool used during development and debugging for printing content to the debugger is:" 
-    "answers" ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+    "answers" :["JavaScript", "terminal/bash", "for loops", "console.log"],
     "correct": 3
-}
+},
 ]
 
 
 // GIVEN I am taking a code quiz
 
 // WHEN I click the start button
+startquiz.addEventListener("click", function() {
+    startingPage. ;
 
+})
 // THEN a timer starts and I am presented with a question
 
 // WHEN I answer a question
